5a00580ef875caee45497d349b34bda9cceabc03
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-12-15 15:56:11 -0800

    Refactor Enigma class: update attribute readers and initialize method for clarity

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,25 @@
 require 'date'
 
 class Enigma
+  attr_reader :encryption,
+              :key,
+              :date
+  def initialize(encryption = '',key = '',date = '')
+    @encryption = encryption.downcase
+    @key     = key
+    @date    = date
+  end
 
+  def encrypt(encryption,key,date)
+    {
+    encryption: rewrite(@encryption),
+    key: @key,
+    date: @date
+   }
+  end
+
+  def rewrite(encryption)
+    
+  end
   
-  # attr_reader :message.
-  #             :key,
-  #             :date
-  # def initialize(message,key,date)
-  #   @message = message.to_lower
-  #   @key     = key
-  #   @date    = date
-  # end
 end
