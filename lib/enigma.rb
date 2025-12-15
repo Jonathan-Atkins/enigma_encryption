f5b94dc0a15eee60a7769226c202e99593ce87d9
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-12-15 15:25:46 -0800

    Implement initial structure for Enigma class and add basic existence test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,14 @@
+require 'date'
+
 class Enigma
+
   
+  # attr_reader :message.
+  #             :key,
+  #             :date
+  # def initialize(message,key,date)
+  #   @message = message.to_lower
+  #   @key     = key
+  #   @date    = date
+  # end
 end
