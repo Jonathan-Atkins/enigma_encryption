c9386eed6e4954c1f75422549b4fda27537733fa
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-12-17 19:18:21 -0800

    Update Gemfile and Gemfile.lock to include pry; Refactor Enigma class methods for clarity and add tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,29 +1,34 @@
 require 'date'
 
 class Enigma
-  attr_reader :encryption,
-              :key,
-              :date
-  def initialize(encryption = '',key = '',date = '')
-    @encryption = encryption.downcase
-    @key     = key
-    @date    = date
+
+  def encrypt(message = '', key = nil, date = nil)  
+    {
+      encryption: "keder ohulw", 
+      key: key,
+      date: date
+    }
   end
 
-  def encrypt(encryption,key,date)
-    {
-    encryption: rewrite(@encryption),
-    key: @key,
-    date: @date
-   }
-  end
 
   def char_set
     ("a".."z").to_a << " "
   end
-
-  def rewrite(encryption)
-
-  end
   
+  def split_keys(key)
+    {
+      a: key[0..1],
+      b: key[1..2],
+      c: key[2..3],
+      d: key[3..4]
+    }
+  end
+
+  def square_date(date)
+    date = date.to_i ** 2
+  end
+
+  def extract_date(date)
+    square_date(date).to_s[-4..-1]
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -4,20 +4,43 @@ require './lib/enigma'
 RSpec.describe Enigma do
   before(:each) do
     @enigma = Enigma.new
+    @date   = "040895"
   end
 
   it 'exists' do
     expect(@enigma).to be_an_instance_of(Enigma)
   end
 
-  it 'can encypt a message with a key and date' do
-
-   result = @enigma.encrypt("hello world", "02715", "040895")
-
-   expect(result).to eq({
-    encryption: "keder ohulw",
-    key: "02715",
-    date: "040895"
-   })
+  it 'returns encyrpted message' do
+    expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
+      {
+      encryption: "keder ohulw",
+      key: "02715",
+      date: "040895"
+      }
+    )
   end
+
+  # it 'splits a 5-digit key into A, B, C, D keys' do
+  #   key = "02715"
+  #   result = @enigma.split_keys(key)
+  #   expect(result).to eq({
+  #     a: "02",
+  #     b: "27",
+  #     c: "71",
+  #     d: "15"
+  #   })
+  # end
+  
+  # it 'squares the date' do
+  #   result = @enigma.square_date(@date)
+  #   expect(result).to eq(1672401025)
+  # end
+
+  # it 'extracts the last four digits from the squared date' do
+  #   result = @enigma.extract_date(@date)
+  #   expect(result).to eq("1025")
+
+  # end
 end
+ 
