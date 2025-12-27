93457da9aa9628e2fa993df2c3b8482f6dfcd695
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-12-27 15:55:26 -0800

    Refactor Enigma class: enhance encryption logic, add key and date checks, and improve tests for message validity

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,19 +2,75 @@ require 'date'
 
 class Enigma
 
-  def encrypt(message = '', key = nil, date = nil)  
+  def encrypt(message = '', key = nil, date = nil)
+    key_str   = key_check(key)
+    date_str  = date_check(date)
+    shifts    = shifts_for(key_str, date_str)
+    encrypted = encrypt_message(message, shifts)
     {
-      encryption: "keder ohulw", 
-      key: key,
-      date: date
+      encryption: encrypted,
+      key: key_str,
+      date: date_str
     }
   end
 
-
-  def char_set
-    ("a".."z").to_a << " "
+  # Encrypts the message, shifting only valid chars, leaving others (including space) in place
+  def encrypt_message(message, shifts)
+    shift_keys  = [:a, :b, :c, :d]
+    shift_index = 0
+    result = message.chars.map do |char|
+      if char_set.include?(char.downcase)
+        shifted = shift_char(char, shifts[shift_keys[shift_index % 4]])
+        shift_index += 1
+        shifted
+      else
+        char
+      end
+    end
+    result.join.downcase
   end
-  
+
+  # Shifts a single character by the given amount, preserving case
+  def shift_char(char, shift)
+    is_upper = char =~ /[A-Z]/
+    base_char = char.downcase
+    if char_set.include?(base_char)
+      idx = char_set.index(base_char)
+      shifted_idx = (idx + shift) % char_set.length
+      shifted_char = char_set[shifted_idx]
+      is_upper ? shifted_char.upcase : shifted_char
+    else
+      char
+    end
+  end
+
+  #!key checks
+  def key_check(key)
+    key.nil? ? self.randomize_key : key
+  end 
+
+  # .rjust(5, "0") pads the string with leading zeros if needed, ensuring it is always 5 characters long.
+  def randomize_key
+    rand(10000..99999).to_s.rjust(5, "0")
+  end
+
+  #*date checks
+
+  def date_check(date)
+    date.nil? ? self.create_date : date
+  end
+
+  def create_date
+    extract_date(Date.today.strftime("%d%m%y"))
+  end
+ 
+  #!Shifts
+  def shifts_for(key, date)
+    keys    = split_keys(key)
+    offsets = split_date(extract_date(date))
+    create_shift(offsets, keys)
+  end
+
   def split_keys(key)
     {
       a: key[0..1],
@@ -24,11 +80,41 @@ class Enigma
     }
   end
 
-  def square_date(date)
-    date = date.to_i ** 2
+  def split_date(date)
+    {
+      a: date[0],
+      b: date[1],
+      c: date[2],
+      d: date[3]
+    }
   end
 
   def extract_date(date)
     square_date(date).to_s[-4..-1]
   end
+
+  def square_date(date)
+    date.to_i ** 2
+  end
+
+  def create_shift(dates,keys)
+    keys.keys.each_with_object({}) do |k, hash|
+      hash[k] = keys[k].to_i + dates[k].to_i
+    end
+  end
+
+  # Returns true if all characters in the message are in the char_set
+
+  def valid_message_chars?(message)
+    message.chars.all? { |char|  char_set.include?(char) }
+  end
+
+  # Converts invalid message characters to a valid char (default: space)
+  # Returns the allowed character set for mes sages
+  def char_set
+    ("a".."z").to_a << " "
+  end
 end
+
+
+
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,6 +2,36 @@ require 'spec_helper'
 require './lib/enigma'
 
 RSpec.describe Enigma do
+
+    it 'encrypts message with spaces and valid chars, leaving spaces in place' do
+      expect(@enigma.encrypt("hello world", "02715", "040895")).to eq(
+        {
+          encryption: "keder ohulw",
+          key: "02715",
+          date: "040895"
+        }
+      )
+    end
+
+    it 'encrypts message with uppercase letters, leaving spaces in place' do
+      expect(@enigma.encrypt("HELLO WORLD", "02715", "040895")).to eq(
+        {
+          encryption: "KEDER OHULW",
+          key: "02715",
+          date: "040895"
+        }
+      )
+    end
+
+    it 'encrypts message with invalid characters, leaving them in place' do
+      expect(@enigma.encrypt("hello world!", "02715", "040895")).to eq(
+        {
+          encryption: "keder ohulw!",
+          key: "02715",
+          date: "040895"
+        }
+      )
+    end
   before(:each) do
     @enigma = Enigma.new
     @date   = "040895"
@@ -21,26 +51,78 @@ RSpec.describe Enigma do
     )
   end
 
-  # it 'splits a 5-digit key into A, B, C, D keys' do
-  #   key = "02715"
-  #   result = @enigma.split_keys(key)
-  #   expect(result).to eq({
-  #     a: "02",
-  #     b: "27",
-  #     c: "71",
-  #     d: "15"
-  #   })
-  # end
+  it 'can check if a key exists' do
+    result = @enigma.encrypt("hello world", "02715", "040895")
+    expect(@enigma.key_check(result[:key])).to eq("02715")
+  end
   
-  # it 'squares the date' do
-  #   result = @enigma.square_date(@date)
-  #   expect(result).to eq(1672401025)
-  # end
+  it 'can create random key' do
+    key = @enigma.randomize_key
+    expect(key).to be_a(String)
+    expect(key.length).to eq(5)
+    expect(key).to match(/\A\d{5}\z/)
+  end
 
-  # it 'extracts the last four digits from the squared date' do
-  #   result = @enigma.extract_date(@date)
-  #   expect(result).to eq("1025")
+  it 'splits a 5-digit key into A, B, C, D keys' do
+    key = "02715"
+    result = @enigma.split_keys(key)
+    expect(result).to eq({
+      a: "02",
+      b: "27",
+      c: "71",
+      d: "15"
+    })
+  end
 
-  # end
+  it 'can check if a date exists' do
+    result = @enigma.encrypt("hello world", "02715", "040895")
+    expect(@enigma.date_check(result[:date])).to eq("040895")
+  end
+
+  it 'can generate todays date' do
+    date = @enigma.create_date
+    expect(date).to be_a(String)
+    expect(date.length).to eq(4)
+    expect(date).to match(/\A\d{4}\z/)
+  end
+
+  it 'squares the date' do
+    expect(@date).to eq("040895")
+    expect(@enigma.square_date(@date)).to eq(1672401025)
+  end
+
+  it 'extracts the last four digits from the squared date' do
+    result = @enigma.extract_date(@date)
+    expect(result.length).to eq(4)
+  end
+
+  it 'splits the date into A,B,C,D keys' do
+    date = @enigma.split_date("1234")
+    
+    expect(date[:a]).to eq("1")
+    expect(date[:b]).to eq("2")
+    expect(date[:c]).to eq("3")
+    expect(date[:d]).to eq("4")
+  end
+
+  it 'creates the shifts' do
+    result = @enigma.encrypt("hello world", "02715", "040895")
+    shifts = @enigma.shifts_for(result[:key],result[:date])
+    
+    expect(shifts).to eq({
+      a: 3,
+      b: 27,
+      c: 73,
+      d: 20
+    })
+  end
+
+  it 'returns true if all message characters are in the char_set' do
+    # expect(@enigma.valid_message_chars?('hello world')).to eq(true)
+    expect(@enigma.valid_message_chars?('hello world!')).to eq(false)
+    expect(@enigma.valid_message_chars?('HELLO')).to eq(false)
+    expect(@enigma.valid_message_chars?('good_morning')).to eq(false)
+    expect(@enigma.valid_message_chars?('abc xyz')).to eq(true)
+  end
 end
  
