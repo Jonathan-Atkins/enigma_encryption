67c272f02c1fb04da742089472810e5848dfc493
Jonathan Atkins <jonathanjatkins89@gmail.com>
2026-03-04 17:18:54 -0800

    updating spec: dates can offset

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,26 @@
 class Enigma
-  
+  def encrypt(message,key=nil,date=nil)
+    {
+      encryption: tokenize(message),
+      key: key,
+      date: offsets(date)
+    }
+  end
+
+  def tokenize(message)
+    message.downcase.split('')
+  end
+
+  def char_set
+    ("a".."z").to_a << " "
+  end
+
+  def squares_date(date)
+    date.to_s.rjust(6, '0')
+    date = date.to_i ** 2
+  end
+
+  def offset(date)
+    "%4d" % (date % 10000)
+  end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -9,4 +9,32 @@ RSpec.describe 'Enigma'do
 
   expect(@enigma).to be_an(Object)
   end
+
+  xit 'can encrypt a message with a key and date' do 
+    expect(@enigma.encrypt("hello world", "02715","040895")).to eq(
+      {
+        encryption: "keder ohulw",
+        key: "02715",
+        date: "040895"
+      })
+  end
+
+  it 'can downcase and split messages' do
+    expect(@enigma.tokenize("HELLO WORLD")).to eq(["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"])
+  end
+
+  it 'can set characters' do 
+    expect(@enigma.char_set).to eq(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "])
+  end
+
+  it 'squares the dates' do
+    date = "040895"
+    expect(@enigma.squares_date(date)).to eq(1672401025)
+  end
+
+  it 'can get last four digits of a number' do
+    num = 1672401025
+
+    expect(@enigma.offset(num)).to eq('1025')
+  end
 end
